1a1275dd13009f36a672210d730c523c9a0be01f
code-with-zen <rajanlakhani@gmail.com>
2021-11-21 19:56:05 -0800

    refactored createFolders.js

--- controller/createFolders.js
+++ controller/createFolders.js
@@ -3,18 +3,17 @@ import fs from "fs";
 export async function createFolders(req, res, next) {
   //create directory
   //create temp dir
-  // const folderName = "333";
   const tempDir = "./upload" + req.body.folderName;
 
   if (!fs.existsSync(tempDir)) {
     fs.mkdirSync(tempDir);
   }
 
-  const childDir = tempDir + "/files/";
+  const childTempDir = tempDir + "/files/";
 
-  if (!fs.existsSync(childDir)) {
-    fs.mkdirSync(childDir);
+  if (!fs.existsSync(childTempDir)) {
+    fs.mkdirSync(childTempDir);
   }
   next();
 }
-//createFolders();
+
