ecff153d7020287ecfba2f88a101800b1afeea58
code-with-zen <rajanlakhani@gmail.com>
2021-11-23 15:58:39 -0800

    refactored cors

--- routes/receiveFilesFromClient.js
+++ routes/receiveFilesFromClient.js
@@ -1,14 +1,30 @@
 import { Router } from "express";
+import cors from "cors";
 import { createFolders } from "../controller/createFolders.js";
 import { createHtmlFile } from "../controller/createHtmlFile.js";
 import { storeFilesToWeb3Storage } from "../controller/storeFilesToWeb3Storage.js";
 import { uploadFilesFromClient } from "../controller/uploadFilesFromClient.js";
 import { body } from "express-validator";
-
 const router = Router();
+
+//const whitelist = [process.env.ORIGIN];
+const whitelist = ['http://localhost:5500'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      console.log("origin: " + origin);
+      callback(null, true);
+    } else {
+      // callback(new Error("Not allowed by CORS"));
+      console.log("origin: " + origin);
+      throw Error("Not allowed by CORS");
+    }
+  },
+};
 
 router.post(
   "/upload-files",
+  cors(corsOptions),
   body("headerTitle").not().isEmpty().trim().escape(),
   body("altImage").not().isEmpty().trim().escape(),
   createFolders,
